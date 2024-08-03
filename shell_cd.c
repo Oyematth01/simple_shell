be621e47320ed18ca065a204e562dbdf2d3bbbf3
Oyematth01 <oyematth@gmail.com>
2024-08-03 10:49:17 +0100

    shell

--- shell_cd.c
+++ shell_cd.c
@@ -1,24 +1,134 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* Function prototypes */
+void execute_command(char **args);
+char *read_line(void);
+char **split_line(char *line);
 
 /**
- * change_directory - changes the current working directory
- * @args: array of arguments
+ * main - Entry point of the shell
  *
- * Description: This function changes the current working directory
- * to the directory specified in the args array. If no directory
- * is specified, it prints an error message.
+ * Return: Always 0 (success)
  */
-void change_directory(char **args)
+int main(void)
 {
-    if (args[1] == NULL)
-    {
-        fprintf(stderr, "cd: expected argument to \"cd\"\n");
-    }
-    else
-    {
-        if (chdir(args[1]) != 0)
-        {
-            perror("cd");
-        }
-    }
+	char *line;
+	char **args;
+	int status = 1;
+
+	do {
+		printf("> ");
+		line = read_line();
+		args = split_line(line);
+
+		if (args[0] == NULL)
+		{
+			/* Empty command, do nothing */
+		}
+		else if (strcmp(args[0], "cd") == 0)
+		{
+			change_directory(args);
+		}
+		else
+		{
+			execute_command(args);
+		}
+
+		free(line);
+		free(args);
+	} while (status);
+
+	return (0);
+}
+
+/**
+ * read_line - Reads a line of input from the user
+ *
+ * Return: The line read from input
+ */
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t bufsize = 0;
+
+	getline(&line, &bufsize, stdin);
+	return (line);
+}
+
+/**
+ * split_line - Splits a line into tokens
+ * @line: The line to be split
+ *
+ * Return: An array of tokens
+ */
+char **split_line(char *line)
+{
+	int bufsize = 64, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "Allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, " \t\r\n\a");
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+
+		if (position >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				fprintf(stderr, "Allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		token = strtok(NULL, " \t\r\n\a");
+	}
+	tokens[position] = NULL;
+	return (tokens);
+}
+
+/**
+ * execute_command - Executes a command
+ * @args: Array of arguments
+ */
+void execute_command(char **args)
+{
+	pid_t pid, wpid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		/* Child process */
+		if (execvp(args[0], args) == -1)
+		{
+			perror("execvp");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		/* Error forking */
+		perror("fork");
+	}
+	else
+	{
+		/* Parent process */
+		do {
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 }
