030fac20100086f0643be37d57c4a111f8a66dec
Oyematth01 <oyematth@gmail.com>
2024-08-03 10:55:55 +0100

    shell

--- shell.c
+++ shell.c
@@ -1,83 +1,134 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* Function prototypes */
+void execute_command(char **args);
+char *read_line(void);
+char **split_line(char *line);
 
 /**
  * main - Entry point of the shell
  *
- * Description: This function provides a simple shell interface that can
- * handle built-in commands like `cd`, `help`, and `exit`. It reads input
- * from the user, tokenizes it, and executes the appropriate commands. The
- * shell runs in an infinite loop until the `exit` command is given.
- *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    char *args[64];
-    pid_t pid;
-    int status;
-    int i;
+	char *line;
+	char **args;
+	int status = 1;
 
-    signal(SIGINT, handle_signal);
+	do {
+		printf("> ");
+		line = read_line();
+		args = split_line(line);
 
-    while (1)
-    {
-        printf("$ ");
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            free(line);
-            perror("getline");
-            exit(EXIT_FAILURE);
-        }
+		if (args[0] == NULL)
+		{
+			/* Empty command, do nothing */
+		}
+		else if (strcmp(args[0], "cd") == 0)
+		{
+			change_directory(args);
+		}
+		else
+		{
+			execute_command(args);
+		}
 
-        line[nread - 1] = '\0'; /* Remove newline character */
-        args[0] = strtok(line, " ");
-        for (i = 1; i < 64; i++)
-        {
-            args[i] = strtok(NULL, " ");
-            if (args[i] == NULL)
-                break;
-        }
+		free(line);
+		free(args);
+	} while (status);
 
-        if (args[0] == NULL)
-        {
-            continue; /* Empty command, prompt again */
-        }
+	return (0);
+}
 
-        if (strcmp(args[0], "cd") == 0)
-        {
-            change_directory(args[1]);
-            continue;
-        }
-        else if (strcmp(args[0], "help") == 0)
-        {
-            print_help();
-            continue;
-        }
-        else if (strcmp(args[0], "exit") == 0)
-        {
-            break;
-        }
+/**
+ * read_line - Reads a line of input from the user
+ *
+ * Return: The line read from input
+ */
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t bufsize = 0;
 
-        pid = fork();
-        if (pid == 0)
-        {
-            execvp(args[0], args);
-            perror("execvp");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid < 0)
-        {
-            perror("fork");
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
-    free(line);
-    return (0);
+	getline(&line, &bufsize, stdin);
+	return (line);
+}
+
+/**
+ * split_line - Splits a line into tokens
+ * @line: The line to be split
+ *
+ * Return: An array of tokens
+ */
+char **split_line(char *line)
+{
+	int bufsize = 64, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "Allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, " \t\r\n\a");
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+
+		if (position >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				fprintf(stderr, "Allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		token = strtok(NULL, " \t\r\n\a");
+	}
+	tokens[position] = NULL;
+	return (tokens);
+}
+
+/**
+ * execute_command - Executes a command
+ * @args: Array of arguments
+ */
+void execute_command(char **args)
+{
+	pid_t pid, wpid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		/* Child process */
+		if (execvp(args[0], args) == -1)
+		{
+			perror("execvp");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		/* Error forking */
+		perror("fork");
+	}
+	else
+	{
+		/* Parent process */
+		do {
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 }
